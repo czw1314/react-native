d39733f3660925137c468e0acd48968162e20f91
chenzongwei <1414650023@qq.com>
2019-06-25 09:54:56 +0800

    init

---
+++ js/redux/action.js
@@ -0,0 +1,13 @@
+//公司选择
+export const newCompany=(company)=>({
+    type:'NEW_COMPANY',
+    company:company
+})
+// export const newTaskList=(taskList)=>({
+//     type:'taskList',
+//     data:taskList
+// })
+// export const newUserId=(taskList)=>({
+//     type:'userId',
+//     data:taskList
+// })
