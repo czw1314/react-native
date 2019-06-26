b90c419d92344ef15238045d3d74bae8eb291a21
chenzongwei <1414650023@qq.com>
2019-06-26 18:13:32 +0800

    Merge branch 'master' of https://github.com/czw1314/react-native
    # Conflicts:
    #	js/redux/action.js

--- res/common/login.js
+++ res/common/login.js
@@ -3,13 +3,48 @@ import {View,StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input,Image } from 'react-native-elements';
 class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            borderColor:'#fff',
+            borderColor1:'#86939e'
+        }
+    }
+    focus(input){
+        if(input==='userName'){
+            this.refs.userName.setNativeProps({borderColor:'#fff'})
+            this.setState({borderColor:'#fff'})
+        }
+        else if(input=='password'){
+            this.setState({borderColor1:'rgb(1,160,254)'})
+        }
+        console.log(this.state.borderColor)
+    }
     render(){
         return(
             <View style={styles.container}>
+                <View style={{alignItems:'center'}}>
                 <Image source={require('../images/login_logo.png')} style={styles.login_logo}/>
+                 </View>
                 <Input
-                    placeholder='INPUT WITH ICON'
-                    leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+                    ref={'userName'}
+                    placeholder='请输入手机号'
+                    inputContainerStyle={{borderColor:'#fff'}}
+                    keyboardType={'number-pad'}
+                    style={{borderBottomColor:'#fff'}}
+                    onFocus={()=>this.focus('userName')}
+                    maxLength={11}
+                    // autoFocus={true}
+                    leftIcon={ <Image source={require('../images/phone.png')} style={styles.icon}/>}
+                />
+                <Input
+                    placeholder='密码'
+                    inputContainerStyle={this.state.borderColor1}
+                    onFocus={()=>this.focus('password')}
+                    inputStyle={{borderColor:'#fff'}}
+                    secureTextEntry={true}
+                    // autoFocus={true}
+                    leftIcon={ <Image source={require('../images/password.png')} style={styles.icon}/>}
                 />
             </View>
 
@@ -25,7 +60,12 @@ const styles=StyleSheet.create({
         flex:1
     },
     login_logo:{
+        marginTop:80,
         width:80,
         height:80
+    },
+    icon:{
+        width:20,
+        height:20
     }
 })
